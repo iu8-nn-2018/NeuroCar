5e19f5d6ca4cf638b27270ec4d4cb6a8d862ab32
zverkovboris <orient-bz@mail.ru>
2018-10-17 23:30:54 +0300

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,16 @@
 ip=$(./connect.sh)
 for param in "$@"
 do
+
+	if [ -f "$param" ];
+	then
+	scp "$param" pi@"$ip":/home/pi/Project/"$param"
+	elif [ -d "$param" ];
+	then
+	tar -cvf "$param".tar.gz "$param"
+	scp "$param".tar.gz pi@"$ip":/home/pi/Project/"$param".tar.gz
+	rm "$param".tar.gz
+	sshpass ssh pi@"$ip"  tar -xvf /home/pi/Project/"$param".tar.gz -C /home/pi/Project/
+	sshpass ssh pi@"$ip" rm /home/pi/Project/"$param".tar.gz
+	fi
 done
